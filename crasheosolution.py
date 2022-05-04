ae58c06a2434da13eb15c52b6232be77790b6b98
Dan A Dsr <90704454+Dan178A@users.noreply.github.com>
2022-05-04 15:34:00 -0400

    Create crasheosolution.py

---
+++ crasheosolution.py
@@ -0,0 +1,8 @@
+#para que no crashee el pyqt5
+# def except_hook(cls, exception, traceback):
+#     sys.__excepthook__(cls, exception, traceback)
+
+# if __name__ == "__main__":
+
+#     import sys
+#     sys.excepthook = except_hook
